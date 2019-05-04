9ace71914e76dc7ba11cdb41fff3d9c2e4f22641
A. Moskaljuk <alekseiko@gmail.com>
2019-05-04 15:18:13 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+const router = express.Router();
+const uri = "mongodb+srv://alekseiko97:Dofkrudwod7@cluster0-zy0vf.gcp.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.use(express.static(__dirname + '/semantic'));
+
+var itemsArray = [];
+
+client.connect(err => {
+    if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+    }
+    const collection = client.db("test").collection("items").find();
+    collection.forEach(function(doc, err) {
+
+        if ( err ) console.log(err);
+        console.log(doc);
+        itemsArray.push(doc);
+    });
+    //perform actions on the collection object
+    client.close();
+});
+
+router.get('/', function( req, res ) {
+    console.log(itemsArray.length);
+    res.render('index.ejs', {
+        items: itemsArray
+    });
+});
+
+app.use('/', router);
+
+app.listen(8080, function() {
+    console.log("Application is running on port 8080");
+});
+
